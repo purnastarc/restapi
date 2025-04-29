67e46a7446507898ef73ce004873928c732e4142
purna chandra pattipati <purnastarc@gmail.com>
2025-04-28 22:28:02 -0400

    feat: initial commit

---
+++ restapi/src/main/java/com/example/restapi/model/Employee.java
@@ -0,0 +1,16 @@
+// model/User.java
+package com.example.restapi.model;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String email;
+
+    // Getters and setters
+}
