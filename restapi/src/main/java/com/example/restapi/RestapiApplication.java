09c675e249a305208a062c6795b515c13b8698cd
purna chandra pattipati <purnastarc@gmail.com>
2025-05-05 07:38:36 -0400

    new changes

--- restapi/src/main/java/com/example/restapi/RestapiApplication.java
+++ restapi/src/main/java/com/example/restapi/RestapiApplication.java
@@ -8,7 +8,7 @@ public class RestapiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RestapiApplication.class, args);
-		System.out.println("Hi");
+		System.out.println("Hii");
 	}
 
 }
